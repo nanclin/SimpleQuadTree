411c96d5660ee3496cd0bf00be56f91bccbddcf1
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2018-12-01 14:40:21 +0100

    Simple quad tree implementation.

---
+++ Assets/Code/QuadTree.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class QuadTree {
+
+    private QuadTree Root;
+
+    public QuadTree GetRoot { get { return Root == null ? this : Root; } }
+
+    public QuadTree[] SubTrees;
+    public int Depth;
+    public float TreeHeight;
+    public Vector2 Position;
+    public float Size;
+    public float Value;
+
+    public QuadTree(Vector2 pos, float size, float value, float treeHeight, QuadTree root = null, int depth = 0) {
+        Position = pos;
+        Size = size;
+        Root = root;
+        TreeHeight = treeHeight;
+        Depth = depth;
+    }
+
+    public void SetValue(Vector2 pos, float value, QuadTree root = null, int depth = 0) {
+        // reached leaf
+        if (depth >= root.TreeHeight) {
+            Value = value;
+            return;
+        }
+
+        float x = pos.x;
+        float y = pos.y;
+
+        // split
+        float hs = this.Size * 0.5f; // half size
+        if (SubTrees == null) {
+            SubTrees = new QuadTree[4];
+            Vector2 newPos0 = this.Position + new Vector2(hs, hs);
+            Vector2 newPos1 = this.Position + new Vector2(0, hs);
+            Vector2 newPos2 = this.Position + new Vector2(0, 0);
+            Vector2 newPos3 = this.Position + new Vector2(hs, 0);
+            SubTrees[0] = new QuadTree(newPos0, hs, 0, TreeHeight - 1, root, depth + 1);
+            SubTrees[1] = new QuadTree(newPos1, hs, 0, TreeHeight - 1, root, depth + 1);
+            SubTrees[2] = new QuadTree(newPos2, hs, 0, TreeHeight - 1, root, depth + 1);
+            SubTrees[3] = new QuadTree(newPos3, hs, 0, TreeHeight - 1, root, depth + 1);
+        }
+
+        QuadTree nextSubTree = null;
+        float cx = this.Position.x + hs; // center x
+        float cy = this.Position.y + hs; // center y
+        if (x > cx && y > cy) {
+            // quadrant 1
+            nextSubTree = SubTrees[0];
+        } else if (x <= cx && y > cy) {
+            // quadrant 2
+            nextSubTree = SubTrees[1];
+        } else if (x <= cx && y <= cy) {
+            // quadrant 3
+            nextSubTree = SubTrees[2];
+        } else if (x > cx && y <= cy) {
+            // quadrant 4
+            nextSubTree = SubTrees[3];
+        }
+        nextSubTree.SetValue(pos, value, root, depth + 1);
+    }
+}
---
+++ Assets/Code/QuadTreeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadTreeController : MonoBehaviour {
+
+    [Header("Press return key to generate quadtree")]
+    [SerializeField] [Range(0, 5)] private int TreeHeight = 3;
+    [SerializeField] bool ShowGrid = true;
+    [SerializeField] bool ShowOnlyFullValues = false;
+
+    private QuadTree Tree;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Tree = new QuadTree(Vector2.zero, 1, 0, TreeHeight);
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Tree != null)
+                Tree.SetValue(mousePos, 1, Tree);
+        }
+    }
+
+    void OnDrawGizmos() {
+        if (Tree != null)
+            DrawQuadTree(Tree);
+    }
+
+    private void DrawQuadTree(QuadTree tree) {
+
+        // draw frame
+        if (tree == tree.GetRoot && ShowGrid)
+            Gizmos.DrawWireCube(Vector2.one * tree.Size * 0.5f, Vector3.one * tree.Size);
+
+        float cx = tree.Position.x + tree.Size * 0.5f;
+        float cy = tree.Position.y + tree.Size * 0.5f;
+
+        // draw value
+        float t = tree.Depth / (float) (tree.GetRoot.TreeHeight);
+        float size = Mathf.Lerp(0.2f, 0.02f, t);
+        float transparency = Mathf.Lerp(0.1f, 1, t);
+        if (ShowOnlyFullValues) {
+            if (tree.Value == 1) {
+                Gizmos.color = Color.white * transparency;
+                Gizmos.DrawCube(new Vector2(cx, cy), Vector3.one * size);
+            }
+        } else {
+            Gizmos.color = Color.Lerp(Color.black, Color.white, t) * transparency;
+            Gizmos.DrawCube(new Vector2(cx, cy), Vector3.one * size);
+        }
+        Gizmos.color = Color.white; // reset color
+
+        if (tree.SubTrees != null) {
+            // draw cross
+            if (ShowGrid) {
+                Gizmos.DrawLine(new Vector2(tree.Position.x, cy), new Vector2(tree.Position.x + tree.Size, cy));
+                Gizmos.DrawLine(new Vector2(cx, tree.Position.y), new Vector2(cx, tree.Position.y + tree.Size));
+            }
+
+            // go level deeper
+            DrawQuadTree(tree.SubTrees[0]);
+            DrawQuadTree(tree.SubTrees[1]);
+            DrawQuadTree(tree.SubTrees[2]);
+            DrawQuadTree(tree.SubTrees[3]);
+        }
+    }
+}
